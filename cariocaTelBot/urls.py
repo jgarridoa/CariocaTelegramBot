1a9f90fe57f720f2fbea0cd9d9a6cea23dcb1d41
Jose Garrido <jgarridoa@outlook.com>
2018-02-09 10:13:17 -0300

    cambios a python2.7

--- cariocaTelBot/urls.py
+++ cariocaTelBot/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import patterns, url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^carioca/', include('carioca_game_telegram.urls', namespace='carioca'))
 ]
